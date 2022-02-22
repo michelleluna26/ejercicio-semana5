fcd9799ba9dc3463720b921a516ad7f2f8b997ca
michelleluna26 <lunamichelle2603@gmail.com>
2022-02-21 21:22:34 -0600

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from distutils.log import debug
 from flask import Flask, render_template
+from db import insertNewPerson, selectAllPersons
 
 
 app = Flask(__name__)
@@ -7,7 +8,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    personlist = selectAllPersons()
+    return render_template("home.html", persons=personlist)
+
+
+@app.route("/new")
+def new_person():
+    return render_template("new.html")
+
+
+@app.route("/insert")
+def insert_person():
+    insertNewPerson()
 
 
 if __name__ == "__main__":
