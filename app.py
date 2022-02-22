10b1401593a72c14493833e31283eb51ac1da120
michelleluna26 <lunamichelle2603@gmail.com>
2022-02-21 20:27:12 -0600

    update01

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "esen"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
